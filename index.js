2951c96148eae7586ac7d4aee99baae9b149079a
Vishwesh Vivek Pujari <69425050+vishwesh-pujari@users.noreply.github.com>
2021-06-22 11:21:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ function playAudio(character) {
 
 function addAnimation(key) {
     var button = document.querySelector("." + key); // eg ".w" or ".a"
-    console.log(button);
+    
+    if (button == null) // if any key other than w, s, d etc was pressed
+        return;
+    
     button.classList.add("pressed"); // add the button to the pressed class so that it gets its applied css
 
     setTimeout(function () {
